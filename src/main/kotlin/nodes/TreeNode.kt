aec8ea7c8ef816055be302d76e473752a242d343
Vyacheslav Kochergin <vyacheslav.kochergin1@gmail.com>
2024-03-31 16:39:43 +0300

    fix: make left and right fields of TreeNode class open

--- src/main/kotlin/nodes/TreeNode.kt
+++ src/main/kotlin/nodes/TreeNode.kt
@@ -1,6 +1,6 @@
 package nodes
 
 open class TreeNode<K : Comparable<K>, V, N : TreeNode<K, V, N>>(var key: K, var value: V) {
-    var left: N? = null
-    var right: N? = null
+    open var left: N? = null
+    open var right: N? = null
 }
