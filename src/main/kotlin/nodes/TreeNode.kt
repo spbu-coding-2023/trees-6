45a5887f37555fdc4315f035ca3774fa484680b4
Vyacheslav Kochergin <vyacheslav.kochergin1@gmail.com>
2024-04-11 20:22:44 +0300

    fix: make key, value, left and right of TreeNode.class internal

--- src/main/kotlin/nodes/TreeNode.kt
+++ src/main/kotlin/nodes/TreeNode.kt
@@ -1,6 +1,10 @@
 package nodes
 
-open class TreeNode<K : Comparable<K>, V, N : TreeNode<K, V, N>>(var key: K, var value: V) {
-    open var left: N? = null
-    open var right: N? = null
+open class TreeNode<K : Comparable<K>, V, N : TreeNode<K, V, N>>(
+    internal var key: K,
+    internal var value: V
+) {
+    internal open var left: N? = null
+    internal open var right: N? = null
 }
+
